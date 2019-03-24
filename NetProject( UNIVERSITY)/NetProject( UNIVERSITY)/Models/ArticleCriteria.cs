a41184f804fab2bf4707db2f0b963c585d908f09
StepanLozinsky <31931306+StepanLozinsky@users.noreply.github.com>
2019-03-24 14:25:43 +0200

    Update ArticleCriteria.cs

--- NetProject( UNIVERSITY)/NetProject( UNIVERSITY)/Models/ArticleCriteria.cs
+++ NetProject( UNIVERSITY)/NetProject( UNIVERSITY)/Models/ArticleCriteria.cs
@@ -12,6 +12,7 @@ namespace NetProject__UNIVERSITY_.Models
 {
     public class ArticleCriteria
     {
+        List<ArticleCriteria> article_list = new List<ArticleCriteria>();
         public string DateFormat { get; set; }
         public DateTime Date { get; set; }
         public string Link { get; set; }
@@ -68,7 +69,17 @@ namespace NetProject__UNIVERSITY_.Models
 
 
         }
+        public void dump_news_info(StreamWriter  f, ArticleCriteria link)
+        {
 
+            string info_line = string.Format("\t{0}{1}{2}\n", link,(article_list.Count).ToString(), article_list[0].date_text);
+            f.WriteLine(info_line);
+            foreach (var article in article_list)
+            {
+                info_line = string.Format("\t{0}{1}{2}{3}\n", article.date_text, article.faculty,(article.page).ToString(), article.link);
+                f.WriteLine(info_line);
+            }
+        }
 
         /*
         private void run_cmd(string cmd, string args)
