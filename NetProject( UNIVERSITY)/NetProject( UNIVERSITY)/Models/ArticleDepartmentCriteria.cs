53f0336f2a4e28d89fc4c321f48933ea935aac8b
StepanLozinsky <31931306+StepanLozinsky@users.noreply.github.com>
2019-03-28 13:28:30 +0200

    Add files via upload

---
+++ NetProject( UNIVERSITY)/NetProject( UNIVERSITY)/Models/ArticleDepartmentCriteria.cs
@@ -0,0 +1,24 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetProject__UNIVERSITY_.Models
+{
+    public class ArticleDepartmentCriteria
+    {
+        public bool IsFilter { get; set; }
+
+
+        public bool ISNULL(HtmlNode articleRaw)
+        {
+            if(articleRaw==null)
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
