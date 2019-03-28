1d7366c07bdd04a7ca660286979b9b9612a28f3d
MykolaPMP <31858361+MykolaPMP@users.noreply.github.com>
2019-03-28 13:47:56 +0200

    Update ArticleDepartmentCriteria.cs

--- NetProject( UNIVERSITY)/NetProject( UNIVERSITY)/Models/ArticleDepartmentCriteria.cs
+++ NetProject( UNIVERSITY)/NetProject( UNIVERSITY)/Models/ArticleDepartmentCriteria.cs
@@ -18,6 +18,42 @@ namespace NetProject__UNIVERSITY_.Models
                 return false;
             }
         }
+         public List<string> ExtractFacultyLinks(string filename)
+        {
+            List<string> links = new List<string>();
+
+            using (FileStream fs = File.OpenRead(filename))
+            {
+                using (StreamReader streamReader = new StreamReader(fs, Encoding.UTF8))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        string line = streamReader.ReadLine();
+                        string link1 = line.Trim();
+                        if (link1[0] == '\ufeff')
+                        {
+                            link1 = link1.Substring(1);
+
+                        }
+                        links.Add(link1);
+                    }
+                }
+            }
+            return links;
+        }
+
+        public List<string> ConcatanateLinks(List<string> facultyLinks, string toAdd)
+        {
+            List<string> facultyNews = new List<string>(facultyLinks);
+
+            for (int i = 0; i < facultyNews.Count; i++)
+            {
+                facultyNews[i] = facultyNews[i] + toAdd;
+            }
+
+            return facultyNews;
+        }
+    
 
 
     }
