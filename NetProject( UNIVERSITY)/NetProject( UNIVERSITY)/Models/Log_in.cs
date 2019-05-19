021aa0e2f9904a75df0a0b00e9102be340db7115
orko_horak <31936833+orko98@users.noreply.github.com>
2019-05-19 21:52:08 +0300

    Add files via upload

---
+++ NetProject( UNIVERSITY)/NetProject( UNIVERSITY)/Models/Log_in.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetProject__UNIVERSITY_.Models
+{
+    public class Log_in
+    {
+        [Required(ErrorMessage = "Please enter Login")]
+        [RegularExpression(@"^[a-zA-Z]{1,20}$",
+                 ErrorMessage = "Characters are not allowed.")]
+        public string Login { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter a Password")]
+        [RegularExpression(@"^[a-zA-Z0-9]{1,30}$",
+           ErrorMessage = "Symbols /,*,+,-,!,_  are not allowed.")]
+        public string Password { get; set; }
+
+    }
+}
